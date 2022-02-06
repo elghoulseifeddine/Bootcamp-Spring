f9960b336ecefd315b2cc9c88374c87d81321d3b
Elghoul Seifeddine <elghoulseifeddine@gmail.com>
2022-02-06 16:16:06 +0100

    add css

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,22 +1,31 @@
 package com.sip.ams.controllers;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
-@RequestMapping("/information")
+
+	@RequestMapping("/information")
 	public String info(Model model) {
-	String formation= "FullStack";
-	model.addAttribute("workshop",formation);
+		ArrayList<String> names = new ArrayList<>();
+		names.add("OCA");
+		names.add("OCP");
+		names.add("Spring");
+		names.add("Angular");
+		model.addAttribute("names", names);
+		String formation = "FullStack";
+		model.addAttribute("workshop", formation);
 		return "home/info";
 	}
 
-@RequestMapping("/affichage")
-    public String affiche() {
-	    return "home/affichage";
-    }
+	@RequestMapping("/affichage")
+	public String affiche() {
+		return "home/affichage";
+	}
 
 }
