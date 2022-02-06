7e04a907a92f788e0f2cd6e6ae9c53b517388f7b
Elghoul Seifeddine <elghoulseifeddine@gmail.com>
2022-02-06 15:30:28 +0100

    Adding pass parametre

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,19 +1,22 @@
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 @RequestMapping("/information")
-	public String info() {
+	public String info(Model model) {
+	String formation= "FullStack";
+	model.addAttribute("workshop",formation);
 		return "home/info";
 	}
 
 @RequestMapping("/affichage")
-public String affiche() {
-	return "home/affichage";
-}
+    public String affiche() {
+	    return "home/affichage";
+    }
 
 }
