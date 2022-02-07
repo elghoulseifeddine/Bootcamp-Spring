d9cd798574ccb2737e7351704d12ca59d30d728c
Elghoul Seifeddine <elghoulseifeddine@gmail.com>
2022-02-07 09:23:10 +0100

    add form

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,17 +1,27 @@
 package com.sip.ams.controllers;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+	
+	@PostMapping("/result")
+	@ResponseBody
+	public String resultat(@RequestParam("nom") String n, @RequestParam("email") String e)
+	{
+		return "Vous avez taper un nom : " +n +" un email : "+e;
+	}
 
 	@RequestMapping("/information")
 	public String info(Model model) {
+				
 		ArrayList<String> names = new ArrayList<>();
 		names.add("OCA");
 		names.add("OCP");
